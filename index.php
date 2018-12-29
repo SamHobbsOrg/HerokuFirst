a020f694acd73e90d47741c9d9f424b2b934804d
Sam Hobbs <46226711+SamHobbsOrg@users.noreply.github.com>
2018-12-28 20:39:11 -0800

    Switching to PHP.

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-﻿<?php header( 'Location: /index.html' ) ;  ?>
+﻿<!doctype html>
+<html>
+<head>
+	<title>First Heroku Site</title>
+</head>
+<body>
+	<h1>First Heroku Site</h1>
+	<p>Here is the PHP version.</p>
+&lt;?php header( 'Location: /index.html' ) ;  ?&gt;
+</body>
+</html>
